ce95f7d0bed7f5b7e1dee9c4bc8a415efb7fa003
deathsimon <deathsimon@gmail.com>
2016-04-19 16:16:59 +0800

    finish parsing application monitoring data

--- Container_Number_Manager.cpp
+++ Container_Number_Manager.cpp
@@ -36,15 +36,122 @@ bool monitorDataUpdate() {
 	// [TODO] check if there are monitor data
 	return true;
 }
+template<class T>
+bool map_exist(std::string target, std::map<std::string, T> m) {
+	bool result = false;
+	if (m.find(target) != m.end()) {
+		result = true;
+	}
+	return result;
+}
+
+void go_to_sleep() {
+#ifdef WINDOWS
+	Sleep(T_SLEEP_MS);
+#else
+	usleep(T_SLEEP_MS);
+#endif
+}
+
+bool check_pod_cpu(Json::Value root, rules* r) {
+	bool result = true;
+
+	if (root["CpuUsage"].isNull()) {
+		std::cerr << "\tCPU load information missing" << std::endl;
+		result = false;
+	}
+	else {				
+		double CpuUsage = root["CpuUsage"].asDouble();
+
+		if (map_exist("CpuUsage", (*r))){
+			double threshold = (*r)["CpuUsage"];
+			if (CpuUsage > threshold) {
+				std::cerr << "\tCPU load exceeds threshold" << std::endl;
+				result = false;
+			}
+		}
+	}
+	return result;
+}
+bool check_pod_mem(Json::Value root, rules* r) {
+	bool result = true;
+
+	if (root["MemoryUsage"].isNull()) {
+		std::cerr << "\tMemory information missing" << std::endl;
+		result = false;
+	}
+	else {		
+		double percentage = root["MemoryUsage"].asDouble();
+
+		if (map_exist("MemoryUsage", (*r))){
+			double threshold = (*r)["MemoryUsage"];
+			if (percentage > threshold) {
+				std::cerr << "\tMemory exceeds threshold" << std::endl;
+				result = false;
+			}
+		}
+	}
+	return result;
+}
+bool check_pod_disk(Json::Value root, rules* r) {
+	bool result = true;
+	// [TODO]
+	return result;
+}
+bool chech_pod_network(Json::Value root, rules* r) {
+	bool result = true;
+	// [TODO]
+	return result;
+}
+void check_application_pod(Json::Value pod, rules* r) {
+	std::string pod_id = pod["PodID"].asString();
+	bool normal = true;
+
+	std::cout << "\tpod " << pod_id << "..." << std::endl;
+
+	normal &= check_pod_cpu(pod["Contents"], r);
+	normal &= check_pod_mem(pod["Contents"], r);
+	normal &= check_pod_disk(pod["Contents"], r);
+	normal &= chech_pod_network(pod["Contents"], r);
+
+	if (normal) {
+		std::cout << "Normal" << std::endl;
+	}
+}
+void parse_application(Json::Value application) {
+	std::string application_id = application["ApplicationID"].asString();	
+
+	std::cout << "Checking application " << application_id << "..." << std::endl;
+
+	if (!map_exist(application_id, ruleset_application)) {
+		std::cout << "No rules for this application, pass." << std::endl;
+	}
+	else{
+		rules* r = ruleset_application[application_id];
+
+		if (!application["pod"].isNull()
+			&& !application["pod"]["PodInfo"].isNull()) {
+			Json::Value pods = application["pod"]["PodInfo"];
+
+			for (Json::Value::iterator it = pods.begin(); it != pods.end(); it++) {
+				check_application_pod((*it), r);
+			}
+		}
+	}	
+}
+void analyze_data_applicaiton(Json::Value applications) {
+	for (Json::Value::iterator it = applications.begin(); it != applications.end(); it++){
+		parse_application((*it));
+	}
+}
 
 std::string query_server_type(std::string server_id) {
 	std::string server_type = "";
-	if (type_server.find(server_id) != type_server.end()){
+	if (map_exist(server_id, type_server)) {
 		server_type = type_server[server_id];
 	}
 	return server_type;
 }
-
 void build_server_type_mapping(Json::Value root) {
 	std::string type;
 	std::string name;
@@ -60,73 +167,79 @@ void build_server_type_mapping(Json::Value root) {
 	}
 }
 
-bool check_cpu(Json::Value root, rules* r) {
+bool check_server_cpu(Json::Value root, rules* r) {
 	bool result = true;
 
 	if (root["Load"].isNull() || root["NumberOfCore"].isNull()) {
 		std::cerr << "\tCPU load information missing" << std::endl;
 		result = false;
 	}
-	else{
-		rules::iterator it;
+	else{		
 		double avgLoad = root["Load"].asDouble() / root["NumberOfCore"].asDouble();
 		
-		if ((it = r->find("CPU load")) != r->end()
-			&& (avgLoad > it->second)) {
-			std::cerr << "\tCPU load exceeds threshold" << std::endl;
-			result = false;
+		if (map_exist("CPU load", (*r))){
+			double threshold = (*r)["CPU load"];
+			if (avgLoad > threshold) {
+				std::cerr << "\tCPU load exceeds threshold" << std::endl;
+				result = false;
+			}
 		}
 	}
 	return result;
 }
-bool check_mem(Json::Value root, rules* r) {
+bool check_server_mem(Json::Value root, rules* r) {
 	bool result = true;
 
 	if (root["SizeOfMem"].isNull() || root["CurrUsage"].isNull()) {
 		std::cerr << "\tMemory information missing" << std::endl;
 		result = false;
 	}
-	else {
-		rules::iterator it;
+	else {		
 		double percentage = root["CurrUsage"].asDouble() / root["SizeOfMem"].asDouble();
 
-		if ((it = r->find("Memory")) != r->end()
-			&& (percentage > it->second)) {
-			std::cerr << "\tMemory exceeds threshold" << std::endl;
-			result = false;
+		if (map_exist("Memory", (*r))){
+			double threshold = (*r)["Memory"];
+			if (percentage > threshold) {
+				std::cerr << "\tMemory exceeds threshold" << std::endl;
+				result = false;
+			}
 		}
 	}
 	return result;
 }
-bool check_disk(Json::Value root, rules* r) {
+bool check_server_disk(Json::Value root, rules* r) {
 	bool result = true;
 	// [TODO]
 	return result;
 }
-bool chech_network(Json::Value root, rules* r) {
+bool chech_server_network(Json::Value root, rules* r) {
 	bool result = true;
 	// [TODO]
 	return result;
 }
-
-void parse_server(Json::Value root) {
-	std::string server_id = root["ServerID"].asString();
+void parse_server(Json::Value server) {
+	std::string server_id = server["ServerID"].asString();
 	std::string server_type = query_server_type(server_id);
-	rules* r = ruleset_server[server_type];
-	bool normal = true;
 
 	std::cout << "Checking server " << server_id << "..." << std::endl;
 
-	normal &= check_cpu(root["CoreInfo"], r);
-	normal &= check_mem(root["MemInfo"], r);
-	normal &= check_disk(root["DiskInfo"], r);
-	normal &= chech_network(root["NetworkInfo"], r);
+	if (!map_exist(server_type, ruleset_server)) {
+		std::cout << "No rules for the server, pass." << std::endl;
+	}
+	else {
+		rules* r = ruleset_server[server_type];
+		bool normal = true;
 
-	if (normal) {
-		std::cout << "Normal" << std::endl;
+		normal &= check_server_cpu(server["CoreInfo"], r);
+		normal &= check_server_mem(server["MemInfo"], r);
+		normal &= check_server_disk(server["DiskInfo"], r);
+		normal &= chech_server_network(server["NetworkInfo"], r);
+
+		if (normal) {
+			std::cout << "Normal" << std::endl;
+		}
 	}
 }
-
 void analyze_data_server(Json::Value root) {
 	//int server_amount = root["NumberOfServer"].asInt();
 	Json::Value servers = root["ServerInfo"];
@@ -140,17 +253,8 @@ void analyze_data(Json::Value root) {
 		analyze_data_server(root[STR_SERVER]);
 	}
 	if (!root[STR_APPLICATION].isNull()) {
-		// [TODO]
-		//analyze_data_applicaiton(root["Application"]);
+		analyze_data_applicaiton(root[STR_APPLICATION]);
 	}
-}
-
-void go_to_sleep() {
-#ifdef WINDOWS
-	Sleep(T_SLEEP_MS);
-#else
-	usleep(T_SLEEP_MS);
-#endif
 }
 
 rules* parse_rules(Json::Value root) {
@@ -162,7 +266,6 @@ rules* parse_rules(Json::Value root) {
 	}
 	return thresholds;
 }
-
 void construct_rule_set(Json::Value root, rule_set* rs) {
 	rules* default_rules = new rules();
 
@@ -172,13 +275,12 @@ void construct_rule_set(Json::Value root, rule_set* rs) {
 	for (Json::Value::iterator it = root.begin(); it != root.end(); it++) {
 		std::string item_name = it.name();
 		if ((*it).size() > 0
-			&& rs->find(item_name) == rs->end()){
+			&& !map_exist(item_name, (*rs))){
 			rules* item_rules = parse_rules((*it));
 			rs->insert(ruleset_pair(item_name, item_rules));
 		}
 	}
 }
-
 void construct_rule_sets_from_tree(Json::Value root) {	
 	if (!root[STR_SERVER].isNull()) {
 		construct_rule_set(root[STR_SERVER], &ruleset_server);
@@ -220,7 +322,6 @@ int main(int argc, char *argv[])
 
 	read_success = read_json_tree_from_file(FILE_SERVER, &root);
 	build_server_type_mapping(root);
-
 
 	// [TODO] fork a child as daemon to do the following
 
